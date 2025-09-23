82732e42125acf756a86f02449e1f234ec4f72be
dipali41235 <dipalisingh5950@gmail.com>
2025-09-23 18:05:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ const fetchBtn = document.getElementById('fetchBtn');
       `;
     });
 
-    // Allow Enter key to trigg
+    // Allow Enter key to trig
     urlInput.addEventListener('keypress', (e) => {
       if (e.key === 'Enter') {
         fetchBtn.click();
