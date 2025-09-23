3fc306e7e3ef6110f6dd3e134d54829cd6c3445d
dipali41235 <dipalisingh5950@gmail.com>
2025-09-23 18:07:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ const fetchBtn = document.getElementById('fetchBtn');
       `;
     });
 
-    // Allow Enter k
+    // Allow Enter 
     urlInput.addEventListener('keypress', (e) => {
       if (e.key === 'Enter') {
         fetchBtn.click();
