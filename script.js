d55210883255a62b16597bf283a7849aeeebd0cc
dipali41235 <dipalisingh5950@gmail.com>
2025-09-26 17:07:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ const fetchBtn = document.getElementById('fetchBtn');
         console.error(err);
         showError(err.message);
       } finally {
-        // Re-enable button
+        // Re-enable butto
         fetchBtn.disabled = false;
         fetchBtn.innerHTML = '<span class="material-icons">search</span> Extract Data';
       }
